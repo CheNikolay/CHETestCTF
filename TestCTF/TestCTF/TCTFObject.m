d456d2753d4134d0f8122218e7e2c8cac3e55fc5
Nikolay Che <che_nikolai@mail.ru>
2016-12-01 17:00:28 +0600

    start

---
+++ TestCTF/TestCTF/TCTFObject.m
@@ -0,0 +1,18 @@
+//
+//  TCTFObject.m
+//  TestCTF
+//
+//  Created by Nikolay Che on 01.12.16.
+//  Copyright © 2016 DataEast. All rights reserved.
+//
+
+#import "TCTFObject.h"
+
+@implementation TCTFObject
+
+- (void)lalala
+{
+    NSLog(@"lalala");
+}
+
+@end
---
+++ TestCTF/TestCTF/TestCTF.h
@@ -0,0 +1,19 @@
+//
+//  TestCTF.h
+//  TestCTF
+//
+//  Created by Nikolay Che on 01.12.16.
+//  Copyright © 2016 DataEast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCTF.
+FOUNDATION_EXPORT double TestCTFVersionNumber;
+
+//! Project version string for TestCTF.
+FOUNDATION_EXPORT const unsigned char TestCTFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCTF/PublicHeader.h>
+
+
