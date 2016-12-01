550f555f10d1fcc38eaf029d699fc3a3c3bbf700
Nikolay Che <che_nikolai@mail.ru>
2016-12-01 17:26:23 +0600

    Build

---
+++ TestCTF/Builds/TestCTF.framework/Headers/TCTFObject.h
@@ -0,0 +1,15 @@
+//
+//  TCTFObject.h
+//  TestCTF
+//
+//  Created by Nikolay Che on 01.12.16.
+//  Copyright © 2016 DataEast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCTFObject : NSObject
+
+- (void)lalala;
+
+@end
